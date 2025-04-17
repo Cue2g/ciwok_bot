48674ec674e098cc6073946756dc7b2dc50f4dc1
Gustavo Blanco <Gustavoblancog29@gmail.com>
2025-04-17 15:52:48 -0400

    add healt

--- app.ts
+++ app.ts
@@ -2,7 +2,10 @@ import Telegraf from "telegraf";
 import { TelegrafContext } from "telegraf/typings/context";
 import { commands } from "./src/cmds";
 import mongoose from "mongoose";
-require('dotenv').config();
+require("dotenv").config();
+import express, { Response } from "express";
+const app = express();
+const port = process.env.PORT || 8000;
 
 export interface ContextFix extends TelegrafContext {
   startPayload?: string;
@@ -10,27 +13,36 @@ export interface ContextFix extends TelegrafContext {
 
 ///db
 const userdb = process.env.DB_USER,
-pwdb = process.env.DB_PW,
-namedb = process.env.DB_NAME,
-url = `mongodb+srv://${userdb}:${pwdb}@cluster0.4qxcs.mongodb.net/${namedb}`
+  pwdb = process.env.DB_PW,
+  namedb = process.env.DB_NAME,
+  url = `mongodb+srv://${userdb}:${pwdb}@cluster0.4qxcs.mongodb.net/${namedb}`;
 
 const bot = new Telegraf<ContextFix>(process.env.TOKEN as string);
 
 export default bot;
 commands(bot);
-console.log("UPDATED")
-mongoose.connect(url).then(()=>{
-  console.log(`mongo connected ${namedb}`);
-  bot.launch()
-  .then(() => console.log(`Bot ${process.env.BOTNAME}`))
-  .catch((err) => {
-    console.log(err)
+console.log("UPDATED");
+mongoose
+  .connect(url)
+  .then(() => {
+    console.log(`mongo connected ${namedb}`);
+    bot
+      .launch()
+      .then(() => console.log(`Bot ${process.env.BOTNAME}`))
+      .catch((err) => {
+        console.log(err);
+      });
   })
-}).catch((err) => {
-  console.log(err)
-  console.log(`error connecting to mongo`)
-})
+  .catch((err) => {
+    console.log(err);
+    console.log(`error connecting to mongo`);
+  });
 
-process.once('SIGINT', () => bot.stop())
-process.once('SIGTERM', () => bot.stop())
+process.once("SIGINT", () => bot.stop());
+process.once("SIGTERM", () => bot.stop());
 
+app.get("/", (_, res: Response) => {
+  res.send("bot running");
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
